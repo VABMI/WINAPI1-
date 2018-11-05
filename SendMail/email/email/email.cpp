5a28d7087b8e7042cc0ad4ee18f858e6d08271e7
VABMI <vaxoalavidze97@gmail.com>
2018-11-06 00:30:21 +0400

    Send Message

--- SendMail/email/email/email.cpp
+++ SendMail/email/email/email.cpp
@@ -6,47 +6,101 @@
 #include<stdlib.h>
 #include<sys/socket.h>
 #include<netinet\in.h>
+#include "curl.h"
 
-int _tmain(int argc, _TCHAR* argv[])
+#include <string.h>
+#include <stdio.h>
+ 
+#define from    "<sender@duniya.com>"
+#define to      "<addressee@gmail.com>"
+#define cc      "<info@example.org>"
+ 
+static const char *payload_text[] = {
+  "Date: Mon, 13 Jun 2018 11:30:00 +0100\r\n",
+  "To: " to "\r\n",
+  "From: " from " (Example User)\r\n",
+  "Cc: " cc " (Another example User)\r\n",
+  "Message-ID: <ecd7db36-10ab-437a-9g3a-e652b9458efd@"
+  "rfcpedant.example.org>\r\n",
+  "Subject: Sanding mail via C\r\n",
+  "\r\n",
+  "This mail is being sent by a C program.\r\n",
+  "\r\n",
+  "It connects to the GMail SMTP server, by far, the most popular mail program of all.\r\n",
+  "Which is also probably written in C.\r\n",
+  "To C or not to C..............\r\n",
+  "That is the question.\r\n",
+  NULL
+};
+ 
+struct upload_status {
+  int lines_read;
+};
+ 
+static size_t payload_source(void *ptr, size_t size, size_t nmemb, void *userp)
 {
-
-
-
-
-
-
-
-	return (EXIT_SUCCESS);
+  struct upload_status *upload_ctx = (struct upload_status *)userp;
+  const char *data;
+ 
+  if((size == 0) || (nmemb == 0) || ((size*nmemb) < 1)) {
+    return 0;
+  }
+ 
+  data = payload_text[upload_ctx->lines_read];
+ 
+  if(data) {
+    size_t len = strlen(data);
+    memcpy(ptr, data, len);
+    upload_ctx->lines_read++;
+ 
+    return len;
+  }
+ 
+  return 0;
 }
-
-int ConnectToServer(const char *server_address){
-	int socket_fd=socket(AF_INET,SOCK_STREAM,IPPROTO_IP);
-	struct sockaddr_in addr;
-	addr.sin_family=AF_INET;
-	addr.sin_port=htons(25);
-	if(inet_pton(AF_INET,GetIPAddress(server_address,addr.sin_addr)==1)
-	{
-		 connect(socket_fd,&addr,sizeof(addr));
-
-	}
-	
-	
-	return socket_fd;
-}
-
-
-const char*GetIPAddress( const char* target_domain)
+ 
+int main(void)
 {
-	const char* target_ip;
-	struct in_addr *host_address;
-	
-	struct hostent *raw_list=gethostbyname(target_domain);
-	int i=0;
-
-	for(i;raw_list->h_addr_list[i]!=0;i++){
-		host_address=raw_list->h_addr_list[i];
-		target_ip=inet_ntoa(host_address);
-	}
-	return target_ip;
-
+  CURL *curl;
+  CURLcode res = CURLE_OK;
+  struct curl_slist *recipients = NULL;
+  struct upload_status upload_ctx;
+ 
+  upload_ctx.lines_read = 0;
+ 
+  curl = curl_easy_init();
+  if(curl) {
+ 
+    curl_easy_setopt(curl, CURLOPT_USERNAME, "user");
+    curl_easy_setopt(curl, CURLOPT_PASSWORD, "secret");
+ 
+    curl_easy_setopt(curl, CURLOPT_URL, "smtp://smtp.gmail.com:465");
+ 
+    curl_easy_setopt(curl, CURLOPT_USE_SSL, (long)CURLUSESSL_ALL);
+ 
+    curl_easy_setopt(curl, CURLOPT_CAINFO, "/path/to/certificate.pem");
+ 
+    curl_easy_setopt(curl, CURLOPT_MAIL_FROM, from);
+ 
+    recipients = curl_slist_append(recipients, to);
+    recipients = curl_slist_append(recipients, cc);
+    curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
+ 
+    curl_easy_setopt(curl, CURLOPT_READFUNCTION, payload_source);
+    curl_easy_setopt(curl, CURLOPT_READDATA, &upload_ctx);
+    curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+ 
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+ 
+    res = curl_easy_perform(curl);
+ 
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",curl_easy_strerror(res));
+ 
+    curl_slist_free_all(recipients);
+ 
+    curl_easy_cleanup(curl);
+  }
+ 
+  return (int)res;
 }
